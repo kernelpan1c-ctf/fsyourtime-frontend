1532b09c21369810e5ee04b60fe5244b81b9aed4
Marcel <marcel.bremm@gmx.de>
2015-11-09 10:42:32 +0100

    1

--- www/views/timeManagement/timeMServices.js
+++ www/views/timeManagement/timeMServices.js
@@ -6,14 +6,14 @@ angular.module('app')
 
   .factory('Efforts', function($window) {
     return $http.get('http://backend-dev.kevinott.de/api/modules/student/', {
-      //headers: {'x-session': $window.sessionStorage.token, 'x-key': irgendwas},
-      //moduleid: $scope.course,
-     // studentid: irgendwas
+      headers: {'x-session': $window.sessionStorage.token.mySessionId, 'x-key': $window.sessionStorage.token.userid},
+      moduleid: $scope.course,
+      studentid: $window.sessionStorage.token.matricularnr
     });
   })
 
   .factory('Modules', function($window) {
       return $http.get('http://backend-dev.kevinott.de/api/modules/student/', {
-        //headers: {'x-session': $window.sessionStorage.token, 'x-key': 0}
+        headers: {'x-session': $window.sessionStorage.token.mySessionId, 'x-key': $window.sessionStorage.token.userid}
       });
     })
