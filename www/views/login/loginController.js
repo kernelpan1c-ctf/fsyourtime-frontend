12ebdd398166491e11ef4db80ed83bdddbd701b6
ChrisBa12 <christopher.ba@web.de>
2015-11-09 23:34:28 +0100

    sessionstorage + modules

--- www/views/login/loginController.js
+++ www/views/login/loginController.js
@@ -15,23 +15,27 @@ angular.module('app')
             //alert(syncdata);
 
               UserService.signIn(username, password, syncdata).success(function (data) {
-                //alert(data.id);
-                //alert("Data Privacy = " + data.privacy);
 
                 $window.sessionStorage.mySessionId = data.mySessionId;
                 $window.sessionStorage.userid = data.userid;
                 $window.sessionStorage.matricularnr = data.matricularnr;
                 $window.sessionStorage.privacy = data.privacy;
 
-                alert(sessionStorage.privacy);
+                //alert(sessionStorage.privacy);
+                UserService.getModules(sessionStorage.mySessionId, sessionStorage.userid).success(function(data){
+                  $window.sessionStorage.modulesArray = data;
+                  alert(sessionStorage.modulesArray);
 
-                $rootScope.hide();
-                  if(data.privacy = 'false') {  //Check if privacy was accepted == true
+                  $rootScope.hide();
+                  if(sessionStorage.privacy = 'false') {  //Check if privacy was accepted == true
                     $location.path('/privacy');
                   }else{
                     $location.path('/app/timer');
                   }
-
+                }).error(function(data){
+                  $rootScope.notify('Could not fetch data from backend');
+                  $location.path('/login');
+                });
               }).error(function (status, data) {
                // $rootScope.hide();
                 $rootScope.notify('Invalid Credentials');
