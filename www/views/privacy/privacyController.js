43e06698cf0ff6c253611eef5049f3118065d73d
ChrisBa12 <christopher.ba@web.de>
2015-11-18 19:29:20 +0100

    privacycontroller angepasst an umgenannte Variable

--- www/views/privacy/privacyController.js
+++ www/views/privacy/privacyController.js
@@ -4,7 +4,7 @@ angular.module('app')
     $scope.privacy = function () {
       PrivacyService.acceptPrivacy(sessionStorage.mySessionId, sessionStorage.userid).success(function () {
         $rootScope.hide();
-        sessionStorage.privacy = true;
+        sessionStorage.privacyFlag = true;
         $location.path('/app/timer');
       }).error(function (data) {
         //set privacy = true failed
